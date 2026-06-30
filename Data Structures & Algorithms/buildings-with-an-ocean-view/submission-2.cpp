9626843ddd9c359f3f1efc04baa552c0640b375b
codingveenit <116004094+codingveenit@users.noreply.github.com>
2026-07-01 00:36:21 +0530

    Add: buildings-with-an-ocean-view - submission-2

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-2.cpp
@@ -0,0 +1,70 @@
+// class Solution {
+// public:
+//     vector<int> findBuildings(vector<int>& heights) {
+//         stack<int, vector<int>> st;
+//         int n = heights.size();
+//         for(int i = 0 ; i < n ;i++) {
+//             while(!st.empty() && heights[st.top()] <= heights[i]) {
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
+
+//         vector<int> ans;
+//         while(!st.empty()) {
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+
+//         reverse(ans.begin(), ans.end());
+
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> findBuildings(vector<int>& heights) {
+
+//         vector<int> ans;
+//         int maxHeight = 0;
+
+//         for (int i = heights.size() - 1; i >= 0; i--) {
+
+//             if (heights[i] > maxHeight) {
+//                 ans.push_back(i);
+//                 maxHeight = heights[i];
+//             }
+//         }
+
+//         reverse(ans.begin(), ans.end());
+
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+
+        int n = heights.size();
+
+        vector<int> ans;
+        stack<int, vector<int>> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && heights[st.top()] < heights[i])
+                st.pop();
+
+            if (st.empty())
+                ans.push_back(i);
+
+            st.push(i);
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
